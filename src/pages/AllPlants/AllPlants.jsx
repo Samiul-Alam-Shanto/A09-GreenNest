4c8debee61e049acdf17f958ddc527dcd14a520a
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2025-12-03 00:52:04 +0600

    added the search functionality to all plants page

--- src/pages/AllPlants/AllPlants.jsx
+++ src/pages/AllPlants/AllPlants.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import usePlants from "../../hooks/usePlants";
 import PlantCard from "../../components/PlantCard/PlantCard";
 import AllPlantsLoading from "../../components/Loading/AllPlantsLoading";
+import { useState } from "react";
 
 const AllPlants = () => {
   const { plants, loading } = usePlants();
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value);
+  };
+  const allPlants = plants.filter((p) =>
+    p.plantName.toLowerCase().includes(searchText.toLowerCase())
+  );
   if (loading) return <AllPlantsLoading />;
 
   return (
@@ -16,8 +25,35 @@ const AllPlants = () => {
           Find the perfect green companion for your space.
         </p>
       </div>
+      <div className="mb-5 flex flex-col gap-4 md:flex-row justify-between">
+        <label onChange={handleSearch} className="input ">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" required placeholder="Search" />
+        </label>
+        <select defaultValue="Pick a color" className="select">
+          <option disabled={true}>Sort By</option>
+          <option>Crimson</option>
+          <option>Amber</option>
+          <option>Velvet</option>
+        </select>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {plants.map((plant) => (
+        {allPlants.map((plant) => (
           <PlantCard key={plant.plantId} plant={plant} />
         ))}
       </div>
